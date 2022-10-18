bc85f10441c6b3edbad639daf20fa4207237a86c
unknown <alsrhks0503@office.uos.ac.kr>
2022-10-18 22:25:41 +0900

    Add capture button

--- camera.js
+++ camera.js
@@ -1,5 +1,18 @@
 const video = document.getElementById("camera");
+const captureButton = document.getElementById("capture-image");
+const imageTag = document.getElementById("image");
+captureButton.addEventListener("click", () => {
+  const canvas = document.createElement("canvas");
+  canvas.width = video.clientWidth;
+  canvas.height = video.clientHeight;
 
-navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-  video.srcObject = stream;
+  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+  const dataURL = canvas.toDataURL();
+  imageTag.src = dataURL;
 });
+
+navigator.mediaDevices
+  .getUserMedia({ video: { width: 500, height: 500 } })
+  .then((stream) => {
+    video.srcObject = stream;
+  });
